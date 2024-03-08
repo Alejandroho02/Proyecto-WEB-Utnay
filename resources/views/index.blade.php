3445a53535df3e2c4c14cbc9bcdfb1fe1de76673
Ignacio Hdez <76490439+Alejandroho02@users.noreply.github.com>
2024-03-08 09:58:44 -0700

    Index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,54 @@
+@extends('plantilla')
+
+@section('Navbar')
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(0,0,0);">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="img/Sony_logo.png" alt="" style="margin-right: 10px; width: 100px; height: auto;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <form class="form-inline my-2 my-lg-0"> <!-- Formulario para el campo de búsqueda -->
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Support</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                @if (Route::has('login'))
+                    @auth
+                        <li class="nav-item">
+                            <a href="{{ url('/home') }}" class="nav-link">Home</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="nav-link">Login</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a href="{{ route('register') }}" class="nav-link">Register</a>
+                            </li>
+                        @endif
+                    @endif
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
+@endsection
